652962a3efe3c3315200276b9072e7b4e77f255a
ahmed <ahmedlearn01@gmail.com>
2021-05-29 11:52:36 -0700

    initial commit

---
+++ script/app.js
@@ -0,0 +1,9 @@
+const bar = document.querySelector('.bar');
+const close = document.querySelector('#close');
+const header = document.querySelector('header');
+bar.addEventListener('click',e=>{
+    header.classList.add('sticky');
+})
+close.addEventListener('click',e=>{
+    header.classList.remove('sticky');
+})
